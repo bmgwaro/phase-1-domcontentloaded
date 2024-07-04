f717ad7b056d1f4e8008750468dde8882c677040
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-07-04 22:18:52 +0300

    DOM content loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const newPar=document.querySelector("#text")
+    newPar.textContent="This is really cool!"
+})
